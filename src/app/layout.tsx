32a1f059f24d3802d51664cc78051fea68a46b51
Sherazi <sherazahmad720@gmail.com>
2025-05-02 13:24:21 +0500

    Update package-lock.json dependencies, add ads.txt for Google AdSense, and modify layout and page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7700197525069981"
+        crossOrigin="anonymous"></script>
+
+
+      <meta name="google-adsense-account" content="ca-pub-7700197525069981" >
+      </meta>
       <body>
+
         <div className="flex h-screen w-screen bg-red-400 gap-5">
           <Profile />
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default function Home() {
-   redirect("/about");
+  //  redirect("/about");
   return (
    <div></div>
   );
